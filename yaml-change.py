efd9c7aba5d7911546c68e4d9fefcf3a7d941d80
Tobias Wiens <tobwiens@gmail.com>
2015-03-20 18:33:17 +0100

    Don't print out line endings

--- yaml-change.py
+++ yaml-change.py
@@ -3,5 +3,4 @@ import sys
 for line in sys.stdin.readlines():
 	if str(sys.argv[1]) in line:
 		line = str(sys.argv[1]) + ': ' +str(sys.argv[2])
-	print line
-
+	print line.replace('\n', '').replace('r', '')
